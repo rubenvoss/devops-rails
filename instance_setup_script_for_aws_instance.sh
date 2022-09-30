2c3188f380812ce1721d69969dd3b9d4ca4b1cfa
Ruben Voß <bekertshop@gmail.com>
2022-09-30 17:32:13 +0200

    add wget && exit if cd fails

--- instance_setup_script_for_aws_instance.sh
+++ instance_setup_script_for_aws_instance.sh
@@ -8,14 +8,16 @@ yum install pcre pcre-devel zlib zlib-devel openssl openssl-devel -y
 
 ##
 
-curl http://nginx.org/download/nginx-1.22.0.tar.gz --output nginx-1.22.0.tar.gz
+wget http://nginx.org/download/nginx-1.22.0.tar.gz
 tar -zxvf nginx-1.22.0.tar.gz
-cd nginx-1.22.0
+cd nginx-1.22.0 || exit
 ./configure --sbin-path=/usr/bin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log \
             --http-log-path=/var/log/nginx/access.log --with-pcre --pid-path=/var/run/nginx.pid \
             --with-http_ssl_module
 make
 make install
 
+
 # checks for the nginx version, was it installed?
 nginx -V
+cd ~ || exit
