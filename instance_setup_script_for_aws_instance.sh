712b1f26a5914c10ae1e70edff119032d1b358e2
Ruben Voß <bekertshop@gmail.com>
2022-09-30 16:57:13 +0200

    add setup script for AWS EC2 instances

---
+++ instance_setup_script_for_aws_instance.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /home/ec2-user
+yum update -y
+yum groupinstall 'Development Tools' -y
+yum install pcre pcre-devel zlib zlib-devel openssl openssl-devel -y
+
+##
+
+curl http://nginx.org/download/nginx-1.22.0.tar.gz --output nginx-1.22.0.tar.gz
+tar -zxvf nginx-1.22.0.tar.gz
+cd nginx-1.22.0
+./configure --sbin-path=/usr/bin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log \
+            --http-log-path=/var/log/nginx/access.log --with-pcre --pid-path=/var/run/nginx.pid \
+            --with-http_ssl_module
+make
+make install
